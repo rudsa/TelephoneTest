4fccdd362560c7adf75625f600b5f087a60a32cc
rudsa <rudsa@naver.com>
2021-12-20 02:53:00 +0900

    2021-12-20

--- src/Telephone.java
+++ src/Telephone.java
@@ -1,6 +1,4 @@
 public class Telephone {
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("Test2");
     }
 }
---
+++ src/TelephoneEnum.java
@@ -0,0 +1,14 @@
+public enum TelephoneEnum {
+//    zero(0, button), one(1, button), two(2, button), three(3, button), four(4, button), five(5, button), six(6, button), seven(7, button), eight(8, button), nine(9, button);
+//
+//    private final int button;
+//
+//    TelephoneEnum(int i, int button) {
+//        this.button = button;
+//    }
+//
+//    public int getButton(){
+//        return button;
+//    }
+    zero, one, tow, three, four, five, six, seven, eight, nine;
+}
